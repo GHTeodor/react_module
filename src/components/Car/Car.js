5cdddc370305322246ed137b00cf583bc5df4327
feden2906 <feden2906@gmail.com>
2022-01-17 13:45:46 +0200

    fix

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -3,14 +3,14 @@ import React from 'react';
 import './Car.css';
 
 
-const Car = ({car: {id, model, price, year}, getCarId}) => {
+const Car = ({car: {id, model, price, year}, deleter}) => {
 
     return (
             <div className='car'>
                 <u>model</u>: <i>{model} </i>
                 <u>price</u>: <i>{price} </i>
                 <u>year</u>: <i>{year} </i>
-                <button onClick={()=>getCarId(id)}>X</button>
+                <button onClick={()=>deleter(id)}>X</button>
                 <hr/>
             </div>
     );
