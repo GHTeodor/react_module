6dc9005a35742e0dca4da5166ff0452acb225957
GHTeodor <Theodor.Lviv@gmail.com>
2022-01-20 12:54:12 +0200

    Lesson3

--- src/pages/PostsPage/PostPage/PostCommentsPage/PostCommentsPage.js
+++ src/pages/PostsPage/PostPage/PostCommentsPage/PostCommentsPage.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useLocation, useParams} from "react-router-dom";
-import {post_Service} from "../../../../services/post_Service";
 
+import {post_Service} from "../../../../services/post_Service";
 import PostComments from "../../../../components/Posts/PostComments";
 
 const PostCommentsPage = () => {
