ad5cf4ae65300b28c6f90a8f2441464ec21a19f7
GHTeodor <Theodor.Lviv@gmail.com>
2022-01-27 21:21:30 +0200

    !

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import './App.css';
-import {UseReducer} from "./components/UseReducer";
+import {useReducer} from "react";
 
+import './App.css';
+
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'incA':
+            return {...state, a: state.a + 1};
+        case 'decA':
+            return {...state, a: state.a - 1};
+        case 'incB':
+            return {...state, b: state.b + 1};
+        case 'decB':
+            return {...state, b: state.b - 1};
+        case 'incC':
+            return {...state, c: state.c + 1};
+        case 'decC':
+            return {...state, c: state.c - 1};
+        default:
+            throw new Error("???");
+    }
+    return state;
+}
 
 function App() {
+    const [state, dispatch] = useReducer(reducer, {a: 0, b: 0, c: 0});
+
     return (
-        <div>
-            <UseReducer/>
+        <div className={'dFlex'}>
+            <div className={'border'}>
+                <div className={'center'}>{state.a}</div>
+                <button onClick={() => dispatch({type: 'incA'})}>+1</button>
+                <button onClick={() => dispatch({type: 'decA'})}>-1</button>
+            </div>
+            <div className={'border'}>
+                <div className={'center'}>{state.b}</div>
+                <button onClick={() => dispatch({type: 'incB'})}>+1</button>
+                <button onClick={() => dispatch({type: 'decB'})}>-1</button>
+            </div>
+            <div className={'border'}>
+                <div className={'center'}>{state.c}</div>
+                <button onClick={() => dispatch({type: 'incC'})}>+1</button>
+                <button onClick={() => dispatch({type: 'decC'})}>-1</button>
+            </div>
         </div>
     );
 }
