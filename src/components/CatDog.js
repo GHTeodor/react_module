908d3dc5a4c7afa557e61903f8aace58be1f21fe
GHTeodor <Theodor.Lviv@gmail.com>
2022-01-28 03:03:35 +0200

    useReducer

--- src/components/CatDog.js
+++ src/components/CatDog.js
@@ -4,22 +4,39 @@ import {Reducer} from "./Reducer"
 
 
 const CatDog = () => {
-    const [state, dispatch] = useReducer(Reducer, {cat: "", dog: ""});
+    const [state, dispatch] = useReducer(Reducer, {cat1: "", dog: ""});
     const [cat, setCat] = useState({});
     const [dog, setDog] = useState({});
+
+    const catClick = () => {
+        if (!state.cat)
+            dispatch({type: "newCat", payload: cat.payload});
+        else if (!state.cat1)
+            dispatch({type: "newCat1", payload: cat.payload});
+        else
+            dispatch({type: "newCat2", payload: cat.payload});
+    };
+
+    const dogClick = () => {
+        if (!state.dog)
+            dispatch({type: "newDog", payload: dog.payload});
+        else if (!state.dog1)
+            dispatch({type: "newDog1", payload: dog.payload});
+        else
+            dispatch({type: "newDog2", payload: dog.payload});
+    };
+
 
     return (
         <div className={'wrapper'}>
             <div className={'form'}>
                 <form onSubmit={e => e.preventDefault()}>
-                    <input placeholder="Add Cat" name="cat"
-                           onChange={event =>
-                               setCat({payload: event.target.value})}/>
-                    <button onClick={() => dispatch({type: "newCat", payload: cat.payload})}>Cat</button>
-                    <input placeholder="Add Dog" name="dog"
-                           onChange={event =>
-                               setDog({payload: event.target.value})}/>
-                    <button onClick={() => dispatch({type: "newDog", payload: dog.payload})}>Dog</button>
+                    <input placeholder="Add Cat"
+                           onChange={event => setCat({payload: event.target.value})}/>
+                    <button onClick={catClick}>Cat</button>
+                    <input placeholder="Add Dog"
+                           onChange={event => setDog({payload: event.target.value})}/>
+                    <button onClick={dogClick}>Dog</button>
                 </form>
             </div>
 
@@ -30,11 +47,28 @@ const CatDog = () => {
                     {state.cat}
                     {state.cat &&
                         <button onClick={() => dispatch({type: "deleteCat"})}>X</button>}
+                    <br/>
+                    {state.cat1}
+                    {state.cat1 &&
+                        <button onClick={() => dispatch({type: "deleteCat1"})}>X</button>}
+                    <br/>
+                    {state.cat2}
+                    {state.cat2 &&
+                        <button onClick={() => dispatch({type: "deleteCat2"})}>X</button>}
+
                 </div>
                 <div>
                     {state.dog}
                     {state.dog &&
                         <button onClick={() => dispatch({type: "deleteDog"})}>X</button>}
+                    <br/>
+                    {state.dog1}
+                    {state.dog1 &&
+                        <button onClick={() => dispatch({type: "deleteDog1"})}>X</button>}
+                    <br/>
+                    {state.dog2}
+                    {state.dog2 &&
+                        <button onClick={() => dispatch({type: "deleteDog2"})}>X</button>}
                 </div>
             </div>
         </div>
--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -2,12 +2,30 @@ const Reducer = (state, action) => {
     switch (action.type) {
         case 'newCat':
             return {...state, cat: action.payload};
+        case 'newCat1':
+            return {...state, cat1: action.payload};
+        case 'newCat2':
+            return {...state, cat2: action.payload};
         case 'newDog':
             return {...state, dog: action.payload};
+        case 'newDog1':
+            return {...state, dog1: action.payload};
+        case 'newDog2':
+            return {...state, dog2: action.payload};
         case 'deleteCat':
             return {...state, cat: state.cat = ""};
+        case 'deleteCat1':
+            return {...state, cat1: state.cat = ""};
+        case 'deleteCat2':
+            return {...state, cat2: state.cat = ""};
         case 'deleteDog':
             return {...state, dog: state.dog = ""};
+        case 'deleteDog1':
+            return {...state, dog1: state.dog = ""};
+        case 'deleteDog2':
+            return {...state, dog2: state.dog = ""};
+
+
         default:
             throw new Error("???");
     }
