4cc3102ca56cc230d3b0523b9c9f530cbc3e45d3
GHTeodor <Theodor.Lviv@gmail.com>
2022-01-30 19:49:00 +0200

    Redux

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -27,9 +27,9 @@ const Form = ({carForUpdate: {id, model, price, year}}) => {
 
     return (
         <form onSubmit={handleSubmit(submit)}>
-            <input type="text" placeholder="Model:" {...register('model')}/>
-            <input type="number" placeholder="Price:" {...register('price')}/>
-            <input type="number" placeholder="Year:" {...register('year')}/>
+            <input type="text" style={{"text-transform": "capitalize"}} placeholder="Model:" {...register('model')}/>
+            <input type="number" min="0" max="1000000" placeholder="Price:" {...register('price')}/>
+            <input type="number" min="1990" max="2022" placeholder="Year:" {...register('year')}/>
             <button>{id ? "_" : "+"}</button>
         </form>
     );
